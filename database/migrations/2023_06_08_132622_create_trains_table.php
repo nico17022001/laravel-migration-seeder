be0f78aca38977dfd1deb163e1955384814ca8fe
nico17022001 <nico17022001boolean@gmail.com>
2023-06-08 16:02:05 +0200

    esercitazione migrate finita ora faccio il bonus

---
+++ database/migrations/2023_06_08_132622_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10)->unique();
+            $table->tinyInteger('number_of_carriages');
+            $table->boolean('in_time')->default(1);
+            $table->boolean('deleted')->default (0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
